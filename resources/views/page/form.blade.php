5687fecc0eb6975dc3a47ef982b5c26ea9b77f94
DevMaurice <developermaurice@gmail.com>
2015-11-19 12:44:36 +0300

    Front end formatting

--- resources/views/page/form.blade.php
+++ resources/views/page/form.blade.php
@@ -1,21 +1,25 @@
 	{!! Form::open(['method' => 'POST', 'route' => 'listing.search', 'class' => 'form-inline']) !!}
-				
+					<div class="col-xs-4">
 				 	<div class="form-group @if($errors->first('location')) has-error @endif">
 				        {!! Form::label('location', 'Location', ['class' => 'control-label']) !!}
 				        {!! Form::text('location', null, ['class' => 'form-control','required'=>'required']) !!}
 				        <small class="text-danger">{{ $errors->first('location') }}</small>
 				    </div>
-
+				    </div>
+				    <div class="col-xs-3">
 				    <div class="form-group @if($errors->first('category_id')) has-error @endif ">
 				        {!! Form::label('category_id', 'Category', ['class' => 'control-label']) !!}
 				        {!! Form::select('category_id', array_pluck($categories,'name','id'), 
 				        [null=>'Select One'], ['id' => 'category_id', 'class' => ' form-control','required'=>'required']) !!}
 				        	<small class="text-danger">{{ $errors->first('category_id') }}</small>
 				    </div>
+				    </div>
+				    <div class="col-xs-4">
 				    <div class="form-group @if($errors->first('amount')) has-error @endif">
-				        {!! Form::label('amount', 'Amount', ['class' => 'control-label']) !!}
+				        {!! Form::label('amount', 'Maximum', ['class' => 'control-label']) !!}
 				        	{!! Form::text('amount', null, ['class' => 'form-control','required'=>'required']) !!}
 				        	<small class="text-danger">{{ $errors->first('amount') }}</small>
+				    </div>
 				    </div>
 				
 				    <div class="btn-group pull-right">				       
