f71268ecaf71bd61eaf64baddb27a182215a27c0
Maurice Kuria <developermaurice@gmail.com>
2015-11-27 11:52:58 +0300

    Style check inline edit

--- app/Category.php
+++ app/Category.php
@@ -2,25 +2,24 @@
 
 namespace App;
 
-use App\Listing;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
     /**
-     * Array values that can be mass assigned
+     * Array values that can be mass assigned.
      * 
      * @var array
      */
     protected $fillable = ['name'];
 
     /**
-     * A category contains many listings
+     * A category contains many listings.
      * 
      * @return App\Listing::class 
      */
     public function listings()
     {
-    	return $this->hasMany('App\Listing');
+         return $this->hasMany('App\Listing');
     }
 }
